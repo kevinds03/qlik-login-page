6b0c27fde33ad95a996342b0cc8bcda94f82138b
Kevin Dedy Setiawan <Kevin.DS@infra.core>
2026-08-13 14:27:56 +0700

    Kevin/Fix index.js to allow successful OIDC login

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const axios = require('axios');
 
 const oidc = require('./oidc');
 const { consoleLog } = require('@ngrok/ngrok');
+const { getUserWithAccess } = require('./db');
 
 const app = express();
 
@@ -23,60 +24,60 @@ app.use(session({
 
 app.use('/oidc', oidc.callback());
 
-app.get('/', (req, res) => {
-  res.redirect('/login');
-});
+// app.get('/', (req, res) => {
+//   res.redirect('/login');
+// });
 
-app.post('/login', async (req, res) => {
-  const { nik, pass } = req.body;
+// app.post('/login', async (req, res) => {
+//   const { nik, pass } = req.body;
 
-  try {
-    const response = await axios.post(
-      process.env.AUTH_API_URL,
-      { user: { nik, pass } },
-      { headers: { 'Content-Type': 'application/json' } }
-    );
+//   try {
+//     const response = await axios.post(
+//       process.env.AUTH_API_URL,
+//       { user: { nik, pass } },
+//       { headers: { 'Content-Type': 'application/json' } }
+//     );
 
-    const result = response.data.LoginESS_V2Result; // get API response result
+//     const result = response.data.LoginESS_V2Result; // get API response result
 
-    if (result === 'Sukses') {
-      req.session.loggedIn = true;
-      req.session.username = nik;
+//     if (result === 'Sukses') {
+//       req.session.loggedIn = true;
+//       req.session.username = nik;
 
-      return res.redirect('/dashboard'); // change to redirect to publisher/Qlik
-    } else {
-      // console.log(result); // delete
-      req.session.loginError = result;
-      return res.redirect('/login');
-    }
+//       return res.redirect('/dashboard'); // change to redirect to publisher/Qlik
+//     } else {
+//       // console.log(result); // delete
+//       req.session.loginError = result;
+//       return res.redirect('/login');
+//     }
 
-    req.session.loginError = 'Login gagal. Mohon coba lagi.';
-    return res.redirect('/login');
+//     req.session.loginError = 'Login gagal. Mohon coba lagi.';
+//     return res.redirect('/login');
 
-  } catch (err) {
-    console.error('Auth API error:', err.message);
-    req.session.loginError = 'Authentication service is unavailable. Try again later.';
-    return res.redirect('/login');
-  }
-});
+//   } catch (err) {
+//     console.error('Auth API error:', err.message);
+//     req.session.loginError = 'Authentication service is unavailable. Try again later.';
+//     return res.redirect('/login');
+//   }
+// });
 
-app.get('/login', (req, res) => {
-  console.log('Session on GET /login:', req.session); // delete
-  const error = req.session.loginError || null;
-  req.session.loginError = null; // clear after showing once
-  res.render('login', { error });
-});
+// app.get('/login', (req, res) => {
+//   console.log('Session on GET /login:', req.session); // delete
+//   const error = req.session.loginError || null;
+//   req.session.loginError = null; // clear after showing once
+//   res.render('login', { error });
+// });
 
-app.get('/dashboard', (req, res) => {
-  if (!req.session.loggedIn) {
-    return res.redirect('/login');
-  }
-  res.render('dashboard', { nik: req.session.nik });
-});
+// app.get('/dashboard', (req, res) => {
+//   if (!req.session.loggedIn) {
+//     return res.redirect('/login');
+//   }
+//   res.render('dashboard', { nik: req.session.nik });
+// });
 
-app.get('/logout', (req, res) => {
-  req.session.destroy(() => res.redirect('/login'));
-});
+// app.get('/logout', (req, res) => {
+//   req.session.destroy(() => res.redirect('/login'));
+// });
 
 // --------------- OIDC interaction routes -----------------------------------------------------
 app.get('/interaction/:uid', async (req, res) => {
@@ -85,38 +86,46 @@ app.get('/interaction/:uid', async (req, res) => {
     res.render('login', { error: null, uid: req.params.uid });
   } catch (err) {
     console.error('Interaction error:', err);
-    res.status(400).send('Invalid or expored login session');
+    res.status(400).send('Invalid or expired login session');
   }
 });
 
-app.post('/interaction/:iud/login', async (req, res) => {
+app.post('/interaction/:uid/login', async (req, res) => {
   const { nik, pass } = req.body;
 
+  let response;
   try {
-    const response = await axios.post(
+    response = await axios.post(
       process.env.AUTH_API_URL,
       { user: { nik, pass } },
       { headers: { 'Content-Type': 'application/json' } }
     );
 
-    const result = response.data.LoginESS_V2Result; // get API response result
+    const result = response.data.LoginESS_V2Result != 'gagal' ? response.data.LoginESS_V2Result : null; // get API response result
 
     if (result === 'Sukses') {
+      // Check if user exists in DB (function in db.js)
+      const dbUser = await getUserWithAccess(nik);
+      if(!dbUser) {
+        return res.render('login', {
+          error: 'Akun belum terdaftar, silahkan hubungi administrator.',
+          uid: req.params.uid
+        });
+      }
+      
       const loginResult = { login: { accountId: nik } };
       return await oidc.interactionFinished(req, res, loginResult, { mergeWithLastSubmission: false });
-    } else {
-      // console.log(result); // delete
-      req.session.loginError = result;
-      return res.redirect('/interaction/${interaction.uid}');
     }
+    const error = result ? result : 'Gagal autentikasi. NIK tidak terdaftar';
 
-    req.session.loginError = 'Login gagal. Mohon coba lagi.';
-    return res.redirect('/interaction/${interaction.uid}');
+    return res.render('login', { error, uid: req.params.uid });
 
   } catch (err) {
     console.error('Auth API error:', err.message);
-    req.session.loginError = 'Authentication service is unavailable. Try again later.';
-    return res.redirect('/interaction/${interaction.uid}');
+    return res.render('login', {
+      error: 'Authentication service is unavailable. Try again later.',
+      uid: req.params.uid
+    });
   }
 });
 
